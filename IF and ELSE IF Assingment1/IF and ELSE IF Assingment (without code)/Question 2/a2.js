a414127aaa140c6ad8859f5c629f36afcbc6fe67
IT Course <sufiyan13112012@gmail.com>
2025-11-16 05:39:21 -0800

    Update a2.js

--- IF and ELSE IF Assingment1/IF and ELSE IF Assingment (without code)/Question 2/a2.js
+++ IF and ELSE IF Assingment1/IF and ELSE IF Assingment (without code)/Question 2/a2.js
@@ -1,4 +1,4 @@
-function checkNumberParity(num) {
+var num = +prompt("Enter Number") {
     if (num === 0) {
         console.log("Zero");
     } else if (num % 2 === 0) {
@@ -8,7 +8,7 @@ function checkNumberParity(num) {
     }
 }
 
-function checkLeapYear(year) {
+var year = +prompt("Enter Year") {
     if (year % 400 === 0) {
         console.log("Leap year");
     } else if (year % 4 === 0 && year % 100 !== 0) {
@@ -16,4 +16,5 @@ function checkLeapYear(year) {
     } else {
         console.log("Not a leap year");
     }
+
 }
